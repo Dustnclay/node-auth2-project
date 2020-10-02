7186ff73c767017f145edf61cc086541ef14fcbf
dustin <dustnclaytiner@gmail.com>
2020-10-02 17:25:11 -0500

    started

---
+++ auth/authRouter.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = require('express').Router();
+
+// const Users = require('../users/usermodel.js')
+
+// const {isValid} = require('../users/users-service.js')
+
+router.post('/register', async (req,res,next) => {
+    const 
+}
+
+)
